e336d0bc585e1afcd07a6ef3696b59bd7464333d
Gossfunkel <katherineegoss@gmail.com>
2026-08-28 15:10:57 +0100

    testing environment working to play sound; no audible difference from compressor yet

--- miniaudio.c
+++ miniaudio.c
@@ -6,10 +6,4 @@
 // We are not using miniaudio's built-in audio generation features.
 #define MA_NO_GENERATION
 
-// We are not using miniaudio's built-in file I/O.
-// Instead, we are using Panda3D's MovieCursor that supports the same audio formats as the rest of Panda3D.
-#define MA_NO_WAV
-#define MA_NO_FLAC
-#define MA_NO_MP3
-
 #include "miniaudio.h"
--- test_comp.c
+++ test_comp.c
@@ -8,38 +8,42 @@
 #define SAMPLERATE 48000
 
 int main() {
+    ma_result result;
     ma_engine engine;
     ma_sound sound;
     p3d_compress_node comp_node;
 
     printf("Welcome! Initialising...\n");
 
-    if (ma_engine_init(NULL, &engine) != MA_SUCCESS) {
-        fprintf(stderr, "Failed to initialise engine!\n");
+    if ((result = ma_engine_init(NULL, &engine)) != MA_SUCCESS) {
+        fprintf(stderr, "Failed to initialise engine! Error: %d\n", result);
         return 1;
     }
 
-    if (ma_sound_init_from_file(&engine, "test_tone_2.wav", MA_SOUND_FLAG_NO_DEFAULT_ATTACHMENT, NULL, NULL, &sound) != MA_SUCCESS) {
-        fprintf(stderr, "Failed to initialise sound from file!\n");
+    if ((result = ma_sound_init_from_file(&engine, "test_tone_2.wav", MA_SOUND_FLAG_NO_DEFAULT_ATTACHMENT, NULL, NULL, &sound)) != MA_SUCCESS) {
+        fprintf(stderr, "Failed to initialise sound from file! Error: %d\n", result);
         return 2;
     }
 
     p3d_compress_node_config comp_conf = p3d_compress_node_config_init(CHANNELS, SAMPLERATE, .5f, 4.f, 1.f);
 
-    if (p3d_compress_node_init(&engine.nodeGraph, &comp_conf, NULL, &comp_node)) {
-        fprintf(stderr, "Failed to initialise compressor node!\n");
+    if ((result = p3d_compress_node_init(&engine.nodeGraph, &comp_conf, NULL, &comp_node)) != MA_SUCCESS) {
+        fprintf(stderr, "Failed to initialise compressor node! Error: %d\n", result);
         return 3;
     }
 
-    if (ma_node_attach_output_bus(&sound, 0, &comp_node, 0) != MA_SUCCESS) {
-        fprintf(stderr, "Failed to attach sound to comp node!\n");
+    if ((result = ma_node_attach_output_bus(&sound, 0, &comp_node, 0)) != MA_SUCCESS) {
+        fprintf(stderr, "Failed to attach sound to comp node! Error: %d\n", result);
         return 4;
     }
 
-    if (ma_node_attach_output_bus(&comp_node, 0, ma_node_graph_get_endpoint(&engine.nodeGraph), 0) != MA_SUCCESS) {
-        fprintf(stderr, "Failed to attach comp node to graph endpoint!\n");
+    if ((result = ma_node_attach_output_bus(&comp_node, 0, ma_node_graph_get_endpoint(&engine.nodeGraph), 0)) != MA_SUCCESS) {
+        fprintf(stderr, "Failed to attach comp node to graph endpoint! Error: %d\n", result);
         return 5;
     }
+
+    ma_sound_set_looping(&sound, true);
+    ma_sound_start(&sound);
 
     bool comp_bypassed = false;
     bool shouldClose = false;
