827acd87048661d36ffe25d7c95502a923af5f53
Gossfunkel <katherineegoss@gmail.com>
2026-09-21 16:23:49 +0100

    copying code from p3d's FrameRateMeter to draft a level meter

--- includes/level_node.h
+++ includes/level_node.h
@@ -11,25 +11,42 @@ typedef struct {
 	float h;
 } Box;
 
+typedef std::pair<Box, Box> LevelDisplay;
+
 /* Miniaudio node finds peak and RMS values for each channel,
  	and writes to a public field in a ma_level_data. Panda node 
  	reads from ma_level_meter_node and changes meter appearance 
  	to display levels.
  */
-class LevelMeter : PandaNode {
-	LVecBase2 position;
-	float width    = {0.f};
-	float height   = {0.f};
-	float bar_base = {0.f};
-	ma_engine *engine;
-	ma_node *sound_group;
-	ma_level_meter level_meter = {};
-	std::vector<Box> bars  = {};
-	std::vector<Box> lines = {};
-public:
-	// TODO can i just get the engine and group from the MiniAudioManager?
-	LevelMeter(ma_engine *eng, ma_node *grp, LVecBase2 pos, float w = 25.f, float h = 100.f);
+class LevelMeter : public PandaNode {
+	LVecBase2 _position;
+	float _width;
+	float _height;
+	float _bar_base;
+	PT(GraphicsOutput) _window;
+	PT(DisplayRegion) _display_region;
+	NodePath _root;
+	ma_engine *_engine;
+	ma_node *_sound_group;
+	ma_level_meter _level_meter;
+	size_t _channels;
+	std::vector<LevelDisplay> _bars;
+	ClockObject *_clock_object;
+	double _update_interval;
+	double _last_update;
+
+	PN_stdfloat _last_aspect_ratio;
+	CPT(TransformState) _aspect_ratio_transform;
+PUBLISHED:
+	// can we just get the engine and group from the MiniAudioManager?
+	LevelMeter(MiniAudioManager *mgr, LVecBase2 pos, float w = 25.f, float h = 100.f);
 	~LevelMeter();
+
+	void setup_window(GraphicsOutput *window);
+	void clear_window();
+
+	INLINE GraphicsOutput *get_window() const;
+	INLINE DisplayRegion *get_display_region() const;
 
 	INLINE void enable();
 	INLINE void disable();
@@ -46,8 +63,32 @@ public:
 	LVecBase2 get_position() const;
 	void set_position(LVecBase2 pos);
 
-	INLINE void update();
+	INLINE void set_clock_object(ClockObject *clock_object);
+	INLINE ClockObject *get_clock_object() const;
 
+	INLINE void set_update_interval(double update_interval);
+	INLINE double get_update_interval() const;
+
+	INLINE void update();
+protected:
+	virtual bool cull_callback(CullTraverser *trav, CullTraverserData &data);
+
+public:
+  static TypeHandle get_class_type() {
+    return _type_handle;
+  }
+  static void init_type() {
+    PandaNode::init_type();
+    register_type(_type_handle, "LevelMeter",
+                  PandaNode::get_class_type());
+  }
+  virtual TypeHandle get_type() const {
+    return get_class_type();
+  }
+  virtual TypeHandle force_init_type() {init_type(); return get_class_type();}
+
+private:
+  static TypeHandle _type_handle;
 }
 
 #endif
--- level_node.cxx
+++ level_node.cxx
@@ -1,21 +1,25 @@
 #include "level_node.h"
 
-LevelMeter::LevelMeter(ma_engine *eng, ma_node *grp, LVecBase2 pos, float w = 25.f, float h = 100.f) : 
-		   engine {eng}, sound_group {grp}, position {pos}, width {w}, height {h} {
-	size_t channels = sound_group->config.channels;
-	float barWidth = ((width-5.f)/channels) - .1f;
+LevelMeter::LevelMeter(MiniAudioManager *mgr, LVecBase2 pos, float w = 25.f, float h = 100.f) : 
+		   _engine {mgr->get_engine()}, _sound_group {mgr->get_all_sounds()}, _position {pos}, 
+		   _width {w}, _height {h} {
+	_channels = _sound_group->config.channels;
+	float barWidth = ((_width-5.f)/_channels) - .1f;
 	// temporary value for near bottom of frame
-	pos.y += height - .1f;
-	bar_base = pos.y;
+	pos.y += _height - .1f;
+	_bar_base = pos.y;
 	for (size_t iChannel = 0; iChannel < channels; ++iChannel) {
 		pos.x += barWidth;
-		bars.emplace_back(Box{pos, barWidth, 0.f});
-		lines.emplace_back(Box{pos, barWidth, 0.1f});
+		_bars.emplace_back(LevelDisplay{
+			Box{pos, barWidth, 0.f},
+			Box{pos, barWidth, 0.1f}
+		});
 	}
 	// connect level meter node to group and graph endpoint
 	ma_result result;
-    ma_level_meter_node_config meter_conf = ma_level_meter_node_config_init(CHANNELS, SAMPLERATE);
-    if ((result = ma_level_meter_node_init(&engine->nodeGraph, &meter_conf, NULL, level_meter)) != MA_SUCCESS) {
+    ma_level_meter_node_config meter_conf = 
+    	ma_level_meter_node_config_init(CHANNELS, SAMPLERATE);
+    if ((result = ma_level_meter_node_init(&_engine->nodeGraph, &meter_conf, NULL, _level_meter)) != MA_SUCCESS) {
         if (result == MA_INVALID_ARGS) {
             fprintf(stderr, "Level meter node initialised with incorrect values!\n");
             return;
@@ -23,75 +27,167 @@ LevelMeter::LevelMeter(ma_engine *eng, ma_node *grp, LVecBase2 pos, float w = 25
         fprintf(stderr, "Failed to initialise level meter node! Error: %d\n", result);
         return;
     }
-    if ((result = ma_node_attach_output_bus(sound_group, 0, level_meter, 0)) != MA_SUCCESS) {
+    if ((result = ma_node_attach_output_bus(_sound_group, 0, _level_meter, 0)) != MA_SUCCESS) {
         fprintf(stderr, "Failed to attach sound group to level meter node! Error: %d\n", result);
         return;
     }
-    if ((result = ma_node_attach_output_bus(level_meter, 0, ma_node_graph_get_endpoint(&engine->nodeGraph), 0)) != MA_SUCCESS) {
+    if ((result = ma_node_attach_output_bus(_level_meter, 0, ma_node_graph_get_endpoint(&_engine->nodeGraph), 0)) != MA_SUCCESS) {
         fprintf(stderr, "Failed to attach level meter node to graph endpoint! Error: %d\n", result);
         return;
     }
+	
+	_update_interval = level_meter_update_interval;
+	_last_update = 0.0f;
+	_clock_object = ClockObject::get_global_clock();
+
+	set_align(A_right);
+	set_card_color(0.0f, 0.0f, 0.0f, 0.4);
+	set_card_as_margin(level_meter_side_margins, level_meter_side_margins, 0.1f, 0.0f);
 }
 
 LevelMeter::~LevelMeter() {
 	// disconnect level meter node from group and graph endpoint
-    if (ma_node_attach_output_bus(sound_group, 0, ma_node_graph_get_endpoint(&engine->nodeGraph), 0) != MA_SUCCESS) {
+	disable();
+    if (ma_node_attach_output_bus(_sound_group, 0, ma_node_graph_get_endpoint(&engine->nodeGraph), 0) != MA_SUCCESS) {
         fprintf(stderr, "Failed to toggle node connection!\n");
     }
-	ma_level_meter_node_uninit(level_meter);
+	ma_level_meter_node_uninit(_level_meter);
+	clear_window();
+}
+
+void LevelMeter::setup_window(GraphicsOutput *window) {
+  clear_window();
+
+  _window = window;
+
+  _root = NodePath("level_meter_root");
+  _root.attach_new_node(this);
+
+  CPT(RenderAttrib) dt = DepthTestAttrib::make(DepthTestAttrib::M_none);
+  CPT(RenderAttrib) dw = DepthWriteAttrib::make(DepthWriteAttrib::M_off);
+  _root.node()->set_attrib(dt, 1);
+  _root.node()->set_attrib(dw, 1);
+  _root.set_material_off(1);
+  _root.set_two_sided(1, 1);
+
+  // If we don't set this explicitly, Panda will cause it to be rendered in a
+  // back-to-front cull bin, which will cause the bounding volume to be
+  // computed unnecessarily.  Saves a little bit of overhead.
+  _root.set_bin("unsorted", 0);
+
+  // Create a display region that covers the entire window.
+  _display_region = _window->make_mono_display_region();
+  _display_region->set_sort(frame_rate_meter_layer_sort);
+
+  // Finally, we need a camera to associate with the display region.
+  PT(Camera) camera = new Camera("level_meter_camera");
+  NodePath camera_np = _root.attach_new_node(camera);
+
+  PT(Lens) lens = new OrthographicLens;
+
+  // We choose these values such that we can place the text against (0, 0).
+  static const PN_stdfloat left = -2.0f;
+  static const PN_stdfloat right = 0.0f;
+  static const PN_stdfloat bottom = -2.0f;
+  static const PN_stdfloat top = 0.0f;
+  lens->set_film_size(right - left, top - bottom);
+  lens->set_film_offset((right + left) * 0.5, (top + bottom) * 0.5);
+  lens->set_near_far(-1000, 1000);
+
+  camera->set_lens(lens);
+  camera->set_scene(_root);
+  _display_region->set_camera(camera_np);
+}
+
+/**
+ * Undoes the effect of a previous call to setup_window().
+ */
+void LevelMeter::
+clear_window() {
+  if (_window != nullptr) {
+    _window->remove_display_region(_display_region);
+    _window = nullptr;
+    _display_region = nullptr;
+  }
+  _root = NodePath();
+}
+
+/**
+ * This function will be called during the cull traversal to perform any
+ * additional operations that should be performed at cull time.  This may
+ * include additional manipulation of render state or additional
+ * visible/invisible decisions, or any other arbitrary operation.
+ *
+ * Note that this function will *not* be called unless set_cull_callback() is
+ * called in the constructor of the derived class.  It is necessary to call
+ * set_cull_callback() to indicated that we require cull_callback() to be
+ * called.
+ *
+ * By the time this function is called, the node has already passed the
+ * bounding-volume test for the viewing frustum, and the node's transform and
+ * state have already been applied to the indicated CullTraverserData object.
+ *
+ * The return value is true if this node should be visible, or false if it
+ * should be culled.
+ */
+bool LevelMeter::
+cull_callback(CullTraverser *trav, CullTraverserData &data) {
+  // This triggers when you try to parent a frame rate meter into the scene
+  // graph yourself.  Instead, use setup_window().
+  nassertr(_display_region != nullptr, false);
+
+  Thread *current_thread = trav->get_current_thread();
+
+  // Statistics
+  //PStatTimer timer(_show_fps_pcollector, current_thread);
+
+  // This is probably a good time to check if the aspect ratio on the window
+  // has changed.
+  int width = _display_region->get_pixel_width();
+  int height = _display_region->get_pixel_height();
+  PN_stdfloat aspect_ratio = 1;
+  if (width != 0 && height != 0) {
+    aspect_ratio = (PN_stdfloat)height / (PN_stdfloat)width;
+  }
+
+  // Scale the transform by the calculated aspect ratio.
+  if (aspect_ratio != _last_aspect_ratio) {
+    _aspect_ratio_transform = 
+    	TransformState::make_scale(LVecBase3(aspect_ratio, 1, 1));
+    _last_aspect_ratio = aspect_ratio;
+  }
+  data._net_transform = 
+  	data._net_transform->compose(_aspect_ratio_transform);
+
+  // Check to see if it's time to update.
+  double now = _clock_object->get_frame_time(current_thread);
+  double elapsed = now - _last_update;
+  if (elapsed < 0.0 || elapsed >= _update_interval) {
+    do_update(current_thread);
+  }
+
+  return PandaNode::cull_callback(trav, data);
+}
+
+/**
+ * Resets the text according to the current frame rate.
+ */
+void LevelMeter::
+do_update(Thread *current_thread) {
+	for (auto& [bar,line] : _bars) {
+		// TODO resize bars/lines to LUFS-scaled values?
+		line.pos.y = _bar_base - 
+					 _height * _level_meter.pData[ch]->peak;
+		bar.height = _height * _level_meter.pData[ch]->rms;
+	}
 }
 
 void LevelMeter::enable() {
 	// TODO add to task manager
+	setup_window(_window);
 }
 
 void LevelMeter::disable() {
 	// TODO remove from task manager
-}
-
-void LevelMeter::update() {
-	for (size_t ch = 0; ch < this->channels; ++ch) {
-		// TODO resize bars/lines to LUFS-scaled values?
-		this->lines.at(ch).pos.y = this->bar_base - 
-								   this->height * this->level_meter.pData[ch]->peak;
-		this->bars.at(ch).height = this->height * this->level_meter.pData[ch]->rms;
-	}
-}
-
-void LevelMeter::set_scale(float width, float height) {
-	this->width = width;
-	this->height = height;
-}
-
-void LevelMeter::set_width(float width) {
-	this->width = width;
-}
-
-void LevelMeter::set_height(float height) {
-	this->height = height;
-}
-
-void LevelMeter::set_scale(LVecBase2 scale) {
-	this->width = scale.x;
-	this->height = scale.y;
-}
-
-float LevelMeter::get_width() const {
-	return this->width;
-}
-
-float LevelMeter::get_height() const {
-	return this->height;
-}
-
-LVecBase2 LevelMeter::get_scale() const {
-	return LVecBase2{this->width, this->height};
-}
-
-void LevelMeter::set_position(LVecBase2 pos) {
-	this->pos = pos;
-}
-
-LVecBase2 LevelMeter::get_position() const {
-	return this->pos;
+	clear_window();
 }
